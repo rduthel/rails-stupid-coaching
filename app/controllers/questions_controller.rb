6e64780030f53f7c8b61e68dc269f215588d4ed3
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-12 14:36:24 +0100

    now users can ask a question via a form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,20 @@
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
 
   def answer
     @question = params[:question]
+    coach_answer
+  end
+
+  private
+
+  def coach_answer
+    @answer = if @question == 'I am going to work right now!'
+                'Good job.'
+              elsif @question[-1] == '?'
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
